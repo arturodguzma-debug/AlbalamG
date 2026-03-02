239e720e7cdae82f84f26098368feec80092ba62
arturodguzma-debug <arturodguzma@gmail.com>
2026-03-02 00:49:45 +0000

    Farodeluz_AlbalamG.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
 st.set_page_config(page_title="AlbalamG Global", page_icon="🐆")
-
-idioma = st.sidebar.selectbox("🌐 Idioma", ["Español", "English", "Français", "Português"])
+idioma = st.sidebar.selectbox("🌐 Idioma", ["Español", "English"])
 
 textos = {
-"Español": {"titulo": "🐆 Imperio AlbalamG", "sub": "Faro Universal", "msg": "Ofrenda de Poder", "pago": "$2.00 USD USDT (BEP20)", "boton": "Enviar", "exito": "¡Sello de Lealtad!"},
+"Español": {"titulo": "🐆 Imperio AlbalamG", "sub": "Faro Universal", "msg": "Ofrenda de Poder", "pago": "$2.00 USD USDT (BEP20)", "boton": "Enviar", "exito": "¡Lealtad Sellada!"},
 "English": {"titulo": "🐆 AlbalamG Empire", "sub": "Universal Lighthouse", "msg": "Power Offering", "pago": "$2.00 USD USDT (BEP20)", "boton": "Send", "exito": "Loyalty Sealed!"}
 }
 
@@ -17,7 +16,7 @@ st.write(t["msg"])
 st.info(t["pago"])
 st.code("0x9e513F8C4E5398CDe9f474D28A336A77CF56D01E", language="text")
 
-st .button(t["boton"]):
-st.balloons(    )
-st.success(t["exito"])
+if st.button(t["boton"]):
+    st.balloons()
+    st.success(t["exito"])
     
